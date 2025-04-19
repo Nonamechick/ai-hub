2d10c5a189847e824086c62eebc8dd6d3e64f710
Nonamechick <otajonovxasan350@gmail.com>
2025-04-19 17:08:14 +0500

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Tailwind is working!</h1>
-    </div>
-    </>
+    <div className='text-[100px]'>HUB</div>
   )
 }
 
